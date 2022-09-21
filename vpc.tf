51cc49fc50a149ef246061e17996d02479c0ee19
Thales Macêdo <thalesmacedo1@live.com>
2022-09-21 13:33:33 -0300

    created 2 subnets

--- vpc.tf
+++ vpc.tf
@@ -4,3 +4,23 @@ resource "aws_vpc" "new_vpc" {
     "Name" = "fullcycle-vpc"
   }
 }
+
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "new-subnet-1" {
+  availability_zone = "us-east-1a"
+  vpc_id            = aws_vpc.new_vpc.id
+  cidr_block        = "10.0.0.0/24"
+  tags = {
+    Name = "fullcycle-subnet-1"
+  }
+}
+
+resource "aws_subnet" "new-subnet-2" {
+  availability_zone = "us-east-1b"
+  vpc_id            = aws_vpc.new_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  tags = {
+    Name = "fullcycle-subnet-2"
+  }
+}
