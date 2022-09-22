6089743394d13f17ff1f66e85459dc602fca36a1
Thales Macêdo <thalesmacedo1@live.com>
2022-09-21 21:39:54 -0300

    created security group

---
+++ cluster.tf
@@ -0,0 +1,14 @@
+resource "aws_security_group" "sg" {
+  vpc_id = aws_vpc.new_vpc.id
+  egress {
+    from_port       = 0
+    to_port         = 0
+    protocol        = "-1"
+    cidr_blocks     = ["0.0.0.0/0"]
+    prefix_list_ids = []
+  }
+
+  tags = {
+    Name = "${var.prefix}-sg"
+  }
+}
