32a4f3dd8d5319841e88a868bd3faf2b279d5169
Thales Macêdo <thalesmacedo1@live.com>
2022-09-22 11:11:31 -0300

    adjusted eks variables, outuputs from vpc and eks

--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "prefix" {}
 variable "vpc_cidr_block" {}
 variable "cluster_name" {}
 variable "retention_days" {}
-variable "subnet_ids" {}
 variable "desired_size" {}
 variable "max_size" {}
 variable "min_size" {}
