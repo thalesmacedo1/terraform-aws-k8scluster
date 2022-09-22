efcde94f1e1465a800ee5054870efc7b9cb515a3
Thales Macêdo <thalesmacedo1@live.com>
2022-09-22 10:11:43 -0300

    added missing variables

--- variables.tf
+++ variables.tf
@@ -1,6 +1,8 @@
 variable "prefix" {}
+variable "vpc_cidr_block" {}
 variable "cluster_name" {}
 variable "retention_days" {}
+variable "subnet_ids" {}
 variable "desired_size" {}
 variable "max_size" {}
 variable "min_size" {}
