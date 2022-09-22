2c392fc7083a70ba32fdc7b1b1c5029073a6b561
Thales Macêdo <thalesmacedo1@live.com>
2022-09-22 00:00:12 -0300

    added instance type on eks node group 1

--- nodes.tf
+++ nodes.tf
@@ -36,6 +36,7 @@ resource "aws_eks_node_group" "node-1" {
   node_group_name = "node-1"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = aws_subnet.subnets[*].id
+  instance_types  = ["t3.micro"]
   scaling_config {
     desired_size = var.desired_size
     max_size     = var.max_size
